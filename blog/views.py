12660327488ccf2a97549448d72a43c7b35677dc
EdMass <zairmass@gmail.com>
2021-12-03 16:55:00 -0500

    mejora

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
+from django import forms
 from django.shortcuts import render
 from django.views.generic import View
+from .forms import PostCreateForm
 
 class BlogListView(View):
     def get(self, request, *args, **kwargs):
@@ -7,3 +9,23 @@ class BlogListView(View):
             
         }
         return render(request, 'blog_list.html', context)
+
+class BlogCreateView(View):
+    def get(self, request, *args, **kwargs):
+        form=PostCreateForm()
+        context={
+            'form':form
+        }
+        return render(request, 'blog_create.html', context)
+
+    def post(self, request, *args, **kwargs):
+        if request.method=="POST": 
+            form = PostCreateForm(request.POST)
+            if form.is_valid():
+                title = form.cleaned_data.get('title')
+                content = form.cleaned_data.get('content')
+                
+        context={
+            
+        }
+        return render(request, 'blog_create.html', context)
