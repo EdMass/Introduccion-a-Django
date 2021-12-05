36c3ecde8d3e31df3d40c55b829dea381758ef79
EdMass <zairmass@gmail.com>
2021-12-05 14:58:10 -0500

    Editar y borrar

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,15 @@
 from django import forms
-from django.shortcuts import render
-from django.views.generic import View
+from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse_lazy
+from django.views.generic import View, UpdateView, DeleteView
+from blog.models import Post
 from .forms import PostCreateForm
 
 class BlogListView(View):
     def get(self, request, *args, **kwargs):
+        posts = Post.objects.all()
         context={
-            
+            'posts':posts
         }
         return render(request, 'blog_list.html', context)
 
@@ -24,8 +27,34 @@ class BlogCreateView(View):
             if form.is_valid():
                 title = form.cleaned_data.get('title')
                 content = form.cleaned_data.get('content')
-                
+                p, created = Post.objects.get_or_create(title=title, content=content)
+                p.save()
+                return redirect('blog:home')
         context={
             
         }
         return render(request, 'blog_create.html', context)
+
+
+class BlogDetailView(View):
+        def get(self, request, pk, *args, **kwargs):
+            post= get_object_or_404(Post, pk=pk)
+            context={
+                'post':post
+            }
+            return render(request, 'blog_detail.html', context)
+
+
+class BlogUpdateView(UpdateView):
+    model=Post
+    fields=['title','content']
+    template_name='blog_update.html'
+
+    def get_success_url(self):
+        pk = self.kwargs['pk']
+        return reverse_lazy('blog:detail', kwargs={'pk':pk})
+
+class BlogDeleteView(DeleteView):
+    model=Post
+    template_name='blog_delete.html'
+    success_url=reverse_lazy('blog:home')
